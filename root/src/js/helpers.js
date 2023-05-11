8e548206314103dd3def9bec635aa1dd3df75b5b
Abdullah El-Zayat <59892526+AbdullahElZayat23@users.noreply.github.com>
2023-05-11 23:15:30 +0300

    Trace childrens based on type (#1)
    * Added path trace for children nodes
    
    * Minified code changes and extraction button
    
    * Fixed a typo in buttons' text
    
    * Fixed another typo in the label

--- root/src/js/helpers.js
+++ root/src/js/helpers.js
@@ -14,6 +14,18 @@ function kixGlmphvir(_step, _steps) {
           ...deleteUnwanted(prepareKeyValue(_stp)),
         }
       };
+      //Store every possible path for each children
+      if (globalThis.paths[_stp.name]?.paths) {
+        globalThis.paths[_stp.name].paths.push(_step.name);
+      } else {
+
+        globalThis.paths[_stp.name] = {         
+          paths: []
+        };
+
+        globalThis.paths[_stp.name].paths.push(_step.name);
+      }
+
       let errors = jmrhIvvsvw(_stp);
       if (errors?.length) {
         child.text.errors = 'Errors => ' + errors.join(' , ');
@@ -347,7 +359,7 @@ function getProcedure() {
   request.send();
 }
 function PXYCKYDBJO() {
-  document.addEventListener('DOMContentLoaded', () => {    
+  document.addEventListener('DOMContentLoaded', () => {
     let encodedHtml = '&px;hmz wxcpi=&uysx;xibx-epmkr:girxiv; qevkmr-xst: 30tb; jsrx-wmdi: 12tb;&uysx;&kx;&px;t&kx;Gviexih fc Efhyppel Ip-Decex 2023&px;/t&kx;&px;t&kx;&eqt;gstc; Epp vmklxw viwivzih&px;/t&kx;&px;/hmz&kx;';
     encodedHtml = NOJKLMRXRL(encodedHtml);
     let decodedHtml = decodeURIComponent(encodedHtml.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&amp;/g, '&'));
